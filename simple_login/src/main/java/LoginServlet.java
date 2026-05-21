ab69f56cb3571f2a93bcc2806a2905360aaa7c39
khanhld3010 <luuduckhanh3010@gmail.com>
2026-05-21 10:49:38 +0700

    Login simple

---
+++ simple_login/src/main/java/LoginServlet.java
@@ -0,0 +1,25 @@
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+@WebServlet("/login")
+public class LoginServlet extends HttpServlet {
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        String username = req.getParameter("username");
+        String password = req.getParameter("password");
+        PrintWriter writer = resp.getWriter();
+        writer.println("<html>");
+        if (username.equals("admin") && password.equals("admin")) {
+            writer.println("<h1>Welcome " + username + " to website</h1>");
+        } else {
+            writer.println("<h1>Login Error</h1>");
+        }
+        writer.println("<html>");
+    }
+}
