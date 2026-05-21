95344454fd247e86703809098d38518ff14adbf1
khanhld3010 <luuduckhanh3010@gmail.com>
2026-05-21 10:22:55 +0700

    First Servlet

---
+++ src/main/java/TimeServer.java
@@ -0,0 +1,21 @@
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.Date;
+
+@WebServlet("/TimeServer")
+public class TimeServer extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        PrintWriter writer = resp.getWriter();
+        writer.println("");
+        Date today = new Date();
+        writer.println("<h1>" + today + "</h1>");
+        writer.println("");
+    }
+}
---
+++ src/main/webapp/SecondJSP.jsp
@@ -0,0 +1,17 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: luudu
+  Date: 5/21/2026
+  Time: 10:13 AM
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+<head>
+    <title>secondJSP</title>
+</head>
+<body>
+    <p>time server</p>
+    <a href="TimeServer">Hello Servlet</a>
+</body>
+</html>
